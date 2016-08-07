8bae716a9d49a53fb3d1efa429c36ed70e858e8e
arpitansu <arpit.pandey05@gmail.com>
2016-08-08 00:27:33 +0530

    this is easier pyhton library for beautiful soup 4, please help us grow this

---
+++ bs.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+def prettify(url):
+	connect = requests.get(url)
+	data = connect.content
+	soup = BeautifulSoup(data, "html.parser")
+	prety = soup.prettify
+	return prety
+
+
+def scrapText(url, tag, classIdentity=False, className=False):
+	connect = requests.get(url)
+	data = connect.content
+	soup = BeautifulSoup(data, "html.parser")
+	get_tags = soup.find_all(tag, {classIdentity : className})
+	tolist = []
+	for data in get_tags:
+		tolist.append(data.text)
+		print (tolist[:])
+
